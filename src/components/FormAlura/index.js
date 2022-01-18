a6b3a1f1ebacaf8a61901599f14b74ecd53c1e12
leonardobrizolla <leonardobrizollars@gmail.com>
2022-01-18 15:03:49 -0300

    Created controller form function

--- src/components/FormAlura/index.js
+++ src/components/FormAlura/index.js
@@ -1,11 +1,24 @@
+import { useState } from 'react';
+import { Typography } from '@material-ui/core';
 import { FormEndereco } from '../FormEndereco';
 import { FormPessoal } from '../FormPessoal';
 import { FormUsuario } from '../FormUsuario';
 
-export const FormAlura = ({ onSubmit, validaCpf }) => (
-  <>
-    <FormPessoal onSubmit={onSubmit} validaCpf={validaCpf} />
-    <FormUsuario />
-    <FormEndereco />
-  </>
-);
+export const FormAlura = ({ onSubmit, validaCpf }) => {
+  const [etapaForm, setEtapaForm] = useState(0);
+
+  const formController = (etapa) => {
+    switch (etapa) {
+      case 0:
+        return <FormUsuario />;
+      case 1:
+        return <FormPessoal onSubmit={onSubmit} validaCpf={validaCpf} />;
+      case 2:
+        return <FormEndereco />;
+      default:
+        return <Typography>Erro etapa form</Typography>;
+    }
+  };
+
+  return formController(etapaForm);
+};
