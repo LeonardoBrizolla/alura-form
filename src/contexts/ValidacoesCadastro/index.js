c1ad75d3e41c987ba8379f231acd3b6cb83b8506
leonardobrizolla <leonardobrizollars@gmail.com>
2022-01-18 23:36:32 -0300

    Refactoring context :P

--- src/contexts/ValidacoesCadastro/index.js
+++ src/contexts/ValidacoesCadastro/index.js
@@ -1,3 +1,3 @@
-import React, { createContext } from 'react';
+import { createContext } from 'react';
 
-export const ValidacoesCadastro = React.createContext();
+export const ValidacoesCadastro = createContext();
